950037bdb1cd971c9be288094b9441716808593a
Julien Castiaux <Julien00859@gmail.com>
2016-02-23 19:32:35 +0100

    Gestion des annonces

--- server/carts.js
+++ server/carts.js
@@ -224,12 +224,16 @@ var Whist = function Whist(player1, player2, player3, player4) {
           if ("announce" in message && typeof message.announce === "string") {
             // On vérifie que l'annonce existe
             if (_.contains(allowedAnn, message.announce)) {
+
+              // On vérifie si on est le dernier à annoncer ou pas
+              var amITheLastOneToAnnounce = this.players[player].announce.name === "Premier" || (typeof this.players[this.nextPlayer()].announce.name !== "undefined" && this.players[this.nextPlayer()].announce.name !== "Premier");
+
               // On regarde le type de l'annonce
               switch (message.announce) {
                 case "Passer":
                   if (this.players[player].announce == "Emballage") {
-                    let self = this;
-                    let theOtherPlayer = _.chain(Object.keys(this.players).filter(function(p){return self.players[p].announce.symbol == self.players[player].announce.symbol})).without(player).first();
+                    var self = this;
+                    var theOtherPlayer = _.chain(Object.keys(this.players).filter(function(p){return self.players[p].announce.symbol == self.players[player].announce.symbol})).without(player).first();
                     this.players[theOtherPlayer].announce.name = "Passer";
                     this.players[theOtherPlayer].announce.level = 0;
                     this.players[theOtherPlayer].announce.symbol = undefined;
@@ -243,11 +247,11 @@ var Whist = function Whist(player1, player2, player3, player4) {
                   // Dans le cas d'un solo ou d'en emballage, le joueur doit spécifier deux arguments supplémentaire:
                   // L'enchère de l'annonce (type number)
                   // Le symbole dans lequel il annonce (dont la valeur doit exister)
-                  if (_.contains(message, "announceLevel") && typeof message.announceLevel === "number" && _.contains(message, "announceSymbol") && _.contains(this.symbols, message.announceSymbol)) {
+                  if ("announceLevel" in message && typeof message.announceLevel === "number" && "announceSymbol" in message && _.contains(this.symbols, message.announceSymbol)) {
                     // On vérifie que le joueur possède au moins une carte de ce symbole
-                    if (_.contains(this.players[player].carts.map(function(x){return x.symbol}), message.announceSymbol)) {
+                    if (_.contains(this.players[player].carts.carts.map(function(x){return x.symbol}), message.announceSymbol)) {
                       // On vérifie que le solo annoncé n'est pas un emballage ou inversemment
-                      let self = this;
+                      var self = this;
                       if (_.contains(Object.keys(this.players).map(function(p){return self.players[p].announce.symbol}), message.announceSymbol)) {
                         message.announce = "Emballage";
 
@@ -259,14 +263,26 @@ var Whist = function Whist(player1, player2, player3, player4) {
                       if (message.announce == "Solo") {
                         // Tester si il y a un joueur après nous qui peut emballer (undefined ou premier)
                         // Tester la puissance de la carte
+                        if (!amITheLastOneToAnnounce) {
+                          if (typeof this.players[player].announce.name === "undefined" || this.announces[this.players[player].announce.name].strength + this.players[player].announce.level < this.announces[message.announce].strength + message.announceLevel) {
+                            this.players[player].announce.name = message.announce;
+                            this.players[player].announce.level = message.announceLevel;
+                            this.players[player].symbol = message.announceSymbol;
+
+                          } else {
+                            throw "InvalidAnnounce Cannot announce less";
+                          }
+                        } else {
+                          throw "InvalidAnnounce Last player cannot go solo";
+                        }
                       } else {
-                        // Tester qu'on a l'autorisation de parler. (ou que rien est défini, dans ce cas, on peut)
+                        // Tester qu'on a l'autorisation de parler.
                         if (this.players[player].announce.canTalk) {
 
                           // Tester qu'on a soit
                           // Un joueur en plus de moi-même qui est solo dans ce symbole
                           // Deux joueurs en tout dont moi-même qui ont emballé le même symbole
-                          let playersWithSameSymboleAsMe = Object.keys(this.players).filter(function(p){return self.players[p].announce.symbol == message.announceSymbol});
+                          var playersWithSameSymboleAsMe = Object.keys(this.players).filter(function(p){return self.players[p].announce.symbol == message.announceSymbol});
                           if (playersWithSameSymboleAsMe.length === 1) {
                             // Tester qu'on ne change pas de symbole pour les emballages et en même temps qu'on ne change pas de type d'annonce
                             if (typeof this.players[player].announce.name === "undefined" || this.player[player].announce.symbol === message.symbol) {
@@ -298,13 +314,13 @@ var Whist = function Whist(player1, player2, player3, player4) {
                                 this.players[player].announce.level = message.announceLevel;
                                 this.players[player].symbol = message.announceSymbol;
 
-                                let theOtherPlayer = playersWithSameSymboleAsMe.filter(function(p){return p != player})[0]
+                                var theOtherPlayer = playersWithSameSymboleAsMe.filter(function(p){return p != player})[0]
                                 this.players[theOtherPlayer].announce.name = message.announce;
                                 this.players[theOtherPlayer].announce.level = message.announceLevel;
                                 this.players[theOtherPlayer].symbol = message.announceSymbol;
                               }
                               else {
-                                throw "InvalidAnnounce Cannot announce less"
+                                throw "InvalidAnnounce Cannot announce less";
                               }
                             } else {
                               throw "InvalidAnnounce Cannot change announced symbol";
@@ -324,11 +340,27 @@ var Whist = function Whist(player1, player2, player3, player4) {
                     throw "InvalidMessage Missing announceLevel (or wrong type) or announceSymbol (or wrong value): " + Object.keys(message).join(", ");
                   }
                   break;
+
                 // Toutes les autres annonces
                 default:
-                  this.announce.name = message.announce;
-                  this.announce.level = 0;
-              }
+                  if (this.players[player].announce.name !== "Premier") {
+                    if (typeof this.players[player].announce.name === "undefined" || this.announces[this.players[player].announce.name].strength + this.players[player].announce.level < this.announces[message.announce].strength) {
+                      this.players[player].announce.name = message.announce;
+                      this.players[player].announce.level = 0;
+                      this.players[player].announce.symbol = undefined;
+                      this.players[player].announce.canTalk = true;
+                    } else {
+                      throw "InvalidAnnounce Cannot announce less";
+                    }
+                  } else {
+                    throw "InvalidAnnounce Premier can only announce \"Emballer\""
+                  }
+              } // End Switch
+
+              // Vérifier si il reste soit
+              // Des joueurs n'ayant encore rien dit
+              // Un joueur qui a annoncé premier
+              // Personne... stopper la partie car tout le monde a passé
 
             } else {
               throw "InvalidAnnounce The given announce:\"" + message.announce + "\" isn't in of the following list: " + allowedAnn.join(", ");
@@ -337,16 +369,12 @@ var Whist = function Whist(player1, player2, player3, player4) {
           } else {
             throw "InvalidMessage Missing announce (or wrong types): " + Object.keys(message).join(", ");
           }
-
-          break;
-        case "":
-
-        default:
-
       }
-
     } else {
       throw "NotYourTurn"
     }
   }
 }
+
+module.exports.carts = Carts;
+module.exports.whist = Whist;
